690bf3f21c75b11426f7fef1d4aeb51d20da1f06
Igor Pak <fellicita0@gmail.com>
2018-11-07 17:48:36 +0400

    edit example

--- Example/Example/ViewController.h
+++ Example/Example/ViewController.h
@@ -10,16 +10,4 @@
 
 @interface ViewController : UIViewController
 
-@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
-- (IBAction)segmentDidChange:(id)sender;
-@property (weak, nonatomic) IBOutlet UILabel *selectedSegmentLabel;
-- (IBAction)removeSegment:(id)sender;
-- (IBAction)addSegment:(id)sender;
-- (IBAction)disableSegment:(id)sender;
-- (IBAction)addImage:(id)sender;
-- (IBAction)customizeTheme:(id)sender;
-- (IBAction)toggleArrow:(id)sender;
-- (IBAction)invertArrowDirection:(id)sender;
-- (IBAction)changeArrowPosition:(id)sender;
-
 @end
